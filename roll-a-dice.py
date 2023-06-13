23c58c066ddbd0f57148f33d1abbc00b7f6bdfa8
VitorAugustoAlves <100448405+VitorAugustoAlves@users.noreply.github.com>
2023-06-13 11:54:40 -0300

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,48 @@
+import random
+
+response = int(input("Prescione y para começar e n para parar"))
+
+if responce == y:
+    no = random.radint(1,6)
+
+if no == 1:
+    print("[-----]")
+    print("[     ]")
+    print("[  0  ]")
+    print("[     ]")
+    print("[-----]")
+
+if no == 2:
+    print("[-----]")
+    print("[ 0   ]")
+    print("[     ]")
+    print("[   0 ]")
+    print("[-----]")
+
+if no == 3:
+    print("[-----]")
+    print("[0    ]")
+    print("[  0  ]")
+    print("[    0]")
+    print("[-----]")
+
+if no == 4:
+    print("[-----]")
+    print("[0   0]")
+    print("[     ]")
+    print("[0   0]")
+    print("[-----]")
+
+if no == 5:
+    print("[-----]")
+    print("[0   0]")
+    print("[  0  ]")
+    print("[0   0]")
+    print("[-----]")
+
+if no == 6:
+    print("[-----]")
+    print("[0 0 0]")
+    print("[     ]")
+    print("[0 0 0]")
+    print("[-----]")    
