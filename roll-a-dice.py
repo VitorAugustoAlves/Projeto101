24318d72e8066dfb69987c439e77cc64b9a15a8a
VitorAugustoAlves <100448405+VitorAugustoAlves@users.noreply.github.com>
2024-02-26 16:27:09 -0300

    Add files via upload

--- roll-a-dice.py
+++ roll-a-dice.py
@@ -5,47 +5,47 @@ response = "y"
 while response == "y":
     no = random.randint(1,6)
 
-if no == 1:
-    print("[-----]")
-    print("[     ]")
-    print("[  0  ]")
-    print("[     ]")
-    print("[-----]")
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
 
-if no == 2:
-    print("[-----]")
-    print("[ 0   ]")
-    print("[     ]")
-    print("[   0 ]")
-    print("[-----]")
+    if no == 2:
+        print("[-----]")
+        print("[ 0   ]")
+        print("[     ]")
+        print("[   0 ]")
+        print("[-----]")
 
-if no == 3:
-    print("[-----]")
-    print("[0    ]")
-    print("[  0  ]")
-    print("[    0]")
-    print("[-----]")
+    if no == 3:
+        print("[-----]")
+        print("[0    ]")
+        print("[  0  ]")
+        print("[    0]")
+        print("[-----]")
 
-if no == 4:
-    print("[-----]")
-    print("[0   0]")
-    print("[     ]")
-    print("[0   0]")
-    print("[-----]")
+    if no == 4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
 
-if no == 5:
-    print("[-----]")
-    print("[0   0]")
-    print("[  0  ]")
-    print("[0   0]")
-    print("[-----]")
+    if no == 5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
 
-if no == 6:
-    print("[-----]")
-    print("[0 0 0]")
-    print("[     ]")
-    print("[0 0 0]")
-    print("[-----]")    
+    if no == 6:
+        print("[-----]")
+        print("[0 0 0]")
+        print("[     ]")
+        print("[0 0 0]")
+        print("[-----]")    
 
 response = input("Prescione y para continuar e n para parar.")
 print("\n")
